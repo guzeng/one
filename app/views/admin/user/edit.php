c94f624ab1a5fa37e896b3671a5cf06fc775f5d0
alexLiang86 <869732593@qq.com>
2014-05-26 22:14:47 +0800

    会员表增加字段

--- app/views/admin/user/edit.php
+++ app/views/admin/user/edit.php
@@ -46,16 +46,126 @@
 									<div class="row">
 										<div class="col-md-6">
 											<div class="form-group">
-												<label class="control-label">会员名</label>
-												<input type="text" id="username" name='username' class="form-control" maxLength='20' placeholder="20字符以内" value="<?php echo isset($row)?$row->username:''?>">
+												<label class="control-label"><span class='req'>*</span>会员名</label>
+												<input type="text" id="username" name='username' class="form-control" maxLength='20' placeholder="" value="<?php echo isset($row)?$row->username:''?>">
 												<span class="help-block"></span>
 											</div>
 										</div>
 										<!--/span-->
 										<div class="col-md-6">
 											<div class="form-group">
-												<label class="control-label"><span class='req'>*</span> 姓名</label>
-												<input type="text" id="name" name='name' class="form-control" maxLength='50' placeholder="50字符以内" value="<?php echo isset($row)?$row->name:''?>">
+												<label class="control-label"><span class='req'>*</span> 邮箱地址</label>
+												<input type="text" id="email" name='email' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->email:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<!--/span-->
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"><span class='req'>*</span> 密码</label>
+												<input type="password" id="password" name='password' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->password:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"><span class='req'>*</span> 确认密码</label>
+												<input type="password" id="password_re" name='password_re' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->password:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<!--/span-->
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"><span class='req'>*</span> 积分</label>
+												<input type="text" id="score" name='score' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->score:'0'?>">
+												<span class="help-block" for="score"></span>
+											</div>
+										</div>
+										<!--/span-->
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"><span class='req'>*</span> 等级</label>
+												<input type="text" id="grade" name='grade' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->grade:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"> 推荐人</label>
+												<input type="text" id="reference" name='reference' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->reference:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"> 姓名</label>
+												<input type="text" id="name" name='name' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->name:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"> 手机号码</label>
+												<input type="text" id="phone" name='phone' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->phone:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"> 固定电话</label>
+												<input type="text" id="telephone" name='telephone' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->telephone:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"> 邮编</label>
+												<input type="text" id="post_code" name='post_code' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->post_code:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label"> QQ</label>
+												<input type="text" id="qq" name='qq' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->qq:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<div class="form-group">
+												<label class="control-label"> 地区</label>
+												<input type="text" id="area" name='area' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->area:''?>">
+												<span class="help-block"></span>
+											</div>
+										</div>
+										<!--/span-->
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<div class="form-group">
+												<label class="control-label"> 详细地址</label>
+												<input type="text" id="address" name='address' class="form-control" maxLength='50' placeholder="" value="<?php echo isset($row)?$row->address:''?>">
 												<span class="help-block"></span>
 											</div>
 										</div>
---
+++ contrib/sql/140526.sql
@@ -0,0 +1,23 @@
+﻿
+ALTER TABLE `one_member`   
+  ADD COLUMN `email` varchar(50) NOT NULL DEFAULT ''  COMMENT '电子邮箱';
+ALTER TABLE `one_member`   
+  ADD COLUMN `password` varchar(50) NOT NULL DEFAULT ''  COMMENT '密码';
+ALTER TABLE `one_member`   
+  ADD COLUMN `telephone` varchar(30) NOT NULL DEFAULT ''  COMMENT '固定电话';
+ALTER TABLE `one_member`   
+  ADD COLUMN `phone` varchar(30) NOT NULL DEFAULT ''  COMMENT '手机';
+ALTER TABLE `one_member`   
+  ADD COLUMN `post_code` varchar(10) NOT NULL DEFAULT ''  COMMENT '邮编';
+ALTER TABLE `one_member`   
+  ADD COLUMN `area` int(11) NOT NULL DEFAULT '0'  COMMENT '地区';
+ALTER TABLE `one_member`   
+  ADD COLUMN `qq` varchar(30) NOT NULL DEFAULT ''  COMMENT 'qq';
+ALTER TABLE `one_member`   
+  ADD COLUMN `telephone` varchar(30) NOT NULL DEFAULT ''  COMMENT '启用';
+ALTER TABLE `one_member`   
+  ADD COLUMN `gender` int(11) NOT NULL DEFAULT '0'  COMMENT '性别';
+ALTER TABLE `one_member`   
+  ADD COLUMN `address` varchar(50) NOT NULL DEFAULT ''  COMMENT '详细地址';
+ALTER TABLE `one_member`   
+  ADD COLUMN `money` decimal(2) NOT NULL DEFAULT '0.00'  COMMENT '预付款';
