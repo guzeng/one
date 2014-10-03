50ab322989fb8798e63da0e766a5b38ef90c0bde
guzeng <zeng.gu@qq.com>
2014-10-04 00:14:35 +0800

    item详情页

--- app/controllers/item.php
+++ app/controllers/item.php
@@ -9,12 +9,46 @@ class Item extends CI_Controller {
     function __construct()
     {
         parent::__construct();
+        $this->load->model('product');
+        $this->load->model('product_category');
     }
 	public function index()
 	{
 		$this->load->view('home/item');
 	}
 
+	public function id($id)
+	{
+		if(!$id)
+		{
+			show_error('参数错误',500);
+		}
+		$product = $this->product->get($id);
+		if(!$product)
+		{
+			show_404();
+		}
+		$category = $this->product_category->get($product->cate_id);
+		
+		if($category)
+		{
+			$data['category'] = $category;
+			if($category->parent_id)
+			{
+				$parent = $this->product_category->get($category->parent_id);
+				$data['parent'] = $parent;
+				$data['child'] = $this->product_category->all(array('where'=>array('parent_id'=>$category->parent_id)));	
+			}
+		}
+		$data['product'] = $product;
+		//看了又看
+		$data['view_again'] = $this->product->all(array('status'=>1),'view_num desc','',3);
+		//销售排行
+		$data['goodsale'] = $this->product->all(array('status'=>1),'sale_num desc','',9);
+		//最终购买了
+		$data['last_buy'] = $this->product->all(array('status'=>1),'','',3);
+		$this->load->view('home/item',$data);
+	}
 }
 
 /* End of file item.php */
--- app/views/home/item.php
+++ app/views/home/item.php
@@ -1,11 +1,13 @@
 <?$this->load->view('home/header')?>
 <div class='container'>
+    <?if(isset($category)):?>
     <div class='row'>
         <ol class="breadcrumb top-bar">
-            <li><a href="#">食品饮料</a></li>
-            <li class="active">进口食品</li>
+            <?if(isset($parent)):?><li><?php echo $parent->name?></li><?endif;?>
+            <li class="active"><?php echo $category->name?></li>
         </ol>
     </div>
+    <?endif;?>
 </div>
 <div class='container m-b-20'>
     <div class='row'>
@@ -14,68 +16,69 @@
                 <div class='col-md-5 col-sm-5 col-xs-12 '>
                     <div class='img'>
                         <div class='m-b-20'>
-                            <img src="<?php echo base_url()?>assets/img/home/pinfo.jpg" class='img-responsive'>
+                            <img src="<?php echo $this->product->pic($product->id)?>" style='height:360px;' class='img-responsive'>
                         </div>
                         <div class='text-center m-b-20'>
                             <div class='pinfo-s m-r-20 active'>
-                                <img src="<?php echo base_url()?>assets/img/home/pinfo-s.jpg" class='img-responsive'>
+                                <img src="<?php echo $this->product->pic($product->id,2,'thumb')?>" class='img-responsive'>
                             </div>
                             <div class='pinfo-s m-r-20'>
-                                <img src="<?php echo base_url()?>assets/img/home/pinfo-s.jpg" class='img-responsive'>
+                                <img src="<?php echo $this->product->pic($product->id,3,'thumb')?>" class='img-responsive'>
                             </div>
                             <div class='pinfo-s m-r-20'>
-                                <img src="<?php echo base_url()?>assets/img/home/pinfo-s.jpg" class='img-responsive'>
+                                <img src="<?php echo $this->product->pic($product->id,4,'thumb')?>" class='img-responsive'>
                             </div>
                             <div class='pinfo-s'>
-                                <img src="<?php echo base_url()?>assets/img/home/pinfo-s.jpg" class='img-responsive'>
+                                <img src="<?php echo $this->product->pic($product->id,5,'thumb')?>" class='img-responsive'>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class='col-md-7 col-sm-7 col-xs-12 p-r-0'>
                     <div class='title'>
-                        妮维雅Nivea洁面 男士净油精华亮肤洁面炭泥150ml
+                        <?php echo $product->name?>
                     </div>
                     <div class='price row'>
                         <div class='col-md-2 t'>促销价</div>
                         <div class='col-md-10 p'>
                             <div class='m-b-10'>
-                                <span class='org'>￥19.90</span>
+                                <span class='org'>￥<?php echo sprintf('%.2f',$product->price)?></span>
                                 <span class='des'>卖家优惠</span>
                             </div>
                             <div class='last'>
-                                一口价 ￥10.00
+                                一口价 ￥<?php echo sprintf('%.2f',$product->best_price)?>
                             </div>
                         </div>
                     </div>
                     <div class='ship-info'>配送范围：新会市区付款后三小时内送达</div>
                     <div class='row info m-b-20'>
                         <div class='col-md-4 col-sm-4 col-xs-4 b-r text-center'>
-                            <div class='f-green m-b-10'><strong>83</strong></div>
+                            <div class='f-green m-b-10'><strong><?php echo $product->sale_num?></strong></div>
                             <div>销量</div>
                         </div>
                         <div class='col-md-4 col-sm-4 col-xs-4 b-r text-center'>
-                            <div class='f-red m-b-10'><strong>83</strong></div>
+                            <div class='f-red m-b-10'><strong><?php echo $product->comment_num?></strong></div>
                             <div>累计评价</div>
                         </div>
                         <div class='col-md-4 col-sm-4 col-xs-4 text-center'>
-                            <div class='f-blue m-b-10'><strong>83</strong></div>
+                            <div class='f-blue m-b-10'><strong><?php echo $product->score?></strong></div>
                             <div>送积分</div>
                         </div>
                     </div>
                     <div class='row m-b-20'>
                         <div class='col-md-2'>分类</div>
                         <div class='col-md-10'>
-                            
+                            <?php echo $category->name?>
                         </div>
                     </div>
                     <div class='row m-b-20 h-35'>
                         <div class='col-md-2'>数量</div>
                         <div class='col-md-10 '>
                             <div class="pinfo">
-                                <input type="text" value="80" class=" form-control">
+                                <input type="text" value="<?php echo $product->min_num?>" class=" form-control">
                                 <button class="btn btn-default plus">+</button>
                                 <button class="btn btn-default minus">-</button>
+                                <input type='hidden' id='min_num' value='<?php echo $product->min_num?>' >
                             </div>
                         </div>
                     </div>
@@ -102,9 +105,14 @@
                 </div>
             </div>
             <div class="img">
-                <img class='img-responsive' src="<?php echo base_url();?>assets/img/home/item-r1.jpg">
-                <img class='img-responsive' src="<?php echo base_url();?>assets/img/home/item-r2.jpg">
-                <img class='img-responsive' src="<?php echo base_url();?>assets/img/home/item-r3.jpg"></div>
+                <?if(!empty($view_again)):?>
+                <?foreach($view_again as $key => $item):?>
+                    <a href="<?php echo base_url()?>item/id/<?php echo $item->id?>">
+                    <img class='img-responsive' src="<?php echo $this->product->pic($item->id)?>">
+                    </a>
+                <?endforeach;?>
+                <?endif;?>
+            </div>
             <div class="nav">
                 <span class="pull-left">
                     <img class='img-responsive' src="<?php echo base_url();?>assets/img/portlet-expand-icon2.png"></span>
@@ -119,11 +127,15 @@
         <!-- left -->
         <div class='col-lg-2 col-md-3 col-sm-3 col-xs-12 col-no-padding'>
             <ul class="list-group m-b-10">
-              <li class="list-group-item">Cras justo odio</li>
-              <li class="list-group-item">Dapibus ac facilisis in</li>
-              <li class="list-group-item">Morbi leo risus</li>
-              <li class="list-group-item">Porta ac consectetur ac</li>
-              <li class="list-group-item">Vestibulum at eros</li>
+                <?if(isset($child) && !empty($child)):?>
+                <?foreach($child as $key => $value):?>
+                <li class="list-group-item">
+                    <a href="<?php echo base_url()?>category/index/cate_id/<?php echo $value->id?>">
+                    <?php echo $value->name?>
+                    </a>
+                </li>
+                <?endforeach;?>
+                <?endif;?>
             </ul>
             <!-- ad 1 -->
             <div class='m-b-10'>
@@ -133,132 +145,50 @@
             <div class='b m-b-10'>
                 <div class='p-10 b-b black'>本周销售排行</div>
                 <ul class="list-unstyled c-top10">
+                    <?if(!empty($goodsale)):?>
+                    <?foreach($goodsale as $key => $item):?>
                     <li>
                         <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
+                            <a href="<?php echo base_url()?>item/id/<?php echo $item->id?>">
+                            <img src="<?php echo $this->product->pic($item->id,1,'thumb')?>" class='img-responsive'>
+                            </a>
                         </div>
                         <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
+                            <div class=' '>
+                                <a href="<?php echo base_url()?>item/id/<?php echo $item->id?>">
+                                    <?php echo $item->name?>
+                                </a>
+                            </div>
+                            <div class='price'>￥<?php echo sprintf('%.2f',$item->price)?></div>
                         </div>
                         <div class='clearfix'></div>
                     </li>
-                    <li>
-                        <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
-                        </div>
-                        <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
-                        </div>
-                        <div class='clearfix'></div>
-                    </li>
-                    <li>
-                        <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
-                        </div>
-                        <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
-                        </div>
-                        <div class='clearfix'></div>
-                    </li>
-                    <li>
-                        <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
-                        </div>
-                        <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
-                        </div>
-                        <div class='clearfix'></div>
-                    </li>
-                    <li>
-                        <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
-                        </div>
-                        <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
-                        </div>
-                        <div class='clearfix'></div>
-                    </li>
-                    <li>
-                        <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
-                        </div>
-                        <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
-                        </div>
-                        <div class='clearfix'></div>
-                    </li>
-                    <li>
-                        <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
-                        </div>
-                        <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
-                        </div>
-                        <div class='clearfix'></div>
-                    </li>
-                    <li>
-                        <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
-                        </div>
-                        <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
-                        </div>
-                        <div class='clearfix'></div>
-                    </li>
-                    <li>
-                        <div class='img pull-left'>
-                            <img src="<?php echo base_url();?>assets/img/home/p-s.jpg" class='img-responsive'>
-                        </div>
-                        <div class='name'>
-                            <div class=' '><a>相思果园 正宗无锡阳山水蜜桃1个尝鲜装</a></div>
-                            <div class='price'>￥999.00</div>
-                        </div>
-                        <div class='clearfix'></div>
-                    </li>
+                    <?endforeach;?>
+                    <?endif;?>
                 </ul>
             </div>
 
             <div class='b'>
-                <div class='p-10 b-b black'>看过海产的顾客最终购买了</div>
+                <div class='p-10 b-b black'>看过<?php echo $category->name?>的顾客最终购买了</div>
                 <ul class="list-unstyled c-top10">
+                    <?if(!empty($last_buy)):?>
+                    <?foreach($last_buy as $key => $item):?>
                     <li>
                         <div class='m-b-10'>
-                            <img class='img-responsive' src="<?php echo base_url();?>assets/img/home/p-s2.jpg">
+                            <a href="<?php echo base_url()?>item/id/<?php echo $item->id?>">
+                            <img src="<?php echo $this->product->pic($item->id,1,'thumb')?>" class='img-responsive'>
+                            </a>
                         </div>
                         <div class='m-b-10'>
-                            <a>海晏堂 淡干海参 刺参 干货 80g海参礼盒 淡干海参</a>
+                            <a href="<?php echo base_url()?>item/id/<?php echo $item->id?>">
+                                <?php echo $item->name?>
+                            </a>
                         </div>
-                        <div class='m-b-5 price'>￥999.00</div>
-                        <div><a class='c-9'>已有3人评价</a></div>
+                        <div class='m-b-5 price'>￥<?php echo sprintf('%.2f',$item->price)?></div>
+                        <div><a class='c-9'>已有<?php echo $item->comment_num?>人评价</a></div>
                     </li>
-                    <li>
-                        <div class='m-b-10'>
-                            <img class='img-responsive' src="<?php echo base_url();?>assets/img/home/p-s2.jpg">
-                        </div>
-                        <div class='m-b-10'>
-                            <a>海晏堂 淡干海参 刺参 干货 80g海参礼盒 淡干海参</a>
-                        </div>
-                        <div class='m-b-5 price'>￥999.00</div>
-                        <div><a class='c-9'>已有3人评价</a></div>
-                    </li>
-                    <li>
-                        <div class='m-b-10'>
-                            <img class='img-responsive' src="<?php echo base_url();?>assets/img/home/p-s2.jpg">
-                        </div>
-                        <div class='m-b-10'>
-                            <a>海晏堂 淡干海参 刺参 干货 80g海参礼盒 淡干海参</a>
-                        </div>
-                        <div class='m-b-5 price'>￥999.00</div>
-                        <div><a class='c-9'>已有3人评价</a></div>
-                    </li>
+                    <?endforeach;?>
+                    <?endif;?>
                 </ul>
             </div>
         </div>
@@ -320,8 +250,7 @@
                 </div>
             </div>
             <div class="item-xq-img">
-                <img class='img-responsive' src="<?php echo base_url();?>assets/img/home/item-xq.jpg">
-                <img class='img-responsive' src="<?php echo base_url();?>assets/img/home/item-xq1.jpg">
+                <?php echo $item->info?>
             </div>
         </div>
         <div class='col-md-2 col-sm-2 col-xs-12 pinfo-mid-right ad-box'>
