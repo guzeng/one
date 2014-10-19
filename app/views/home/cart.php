433d583daf2e2385ac9578bf4b7c9000d8c0c902
janson <liujanson@126.com>
2014-10-20 00:08:16 +0800

    购物车

--- app/views/home/cart.php
+++ app/views/home/cart.php
@@ -1,60 +1,108 @@
-<?$this->load->view('home/header')?>
-<div class='container m-t-20' id="mycart">
-  <div class='row'>
-    <!-- left -->
-    <!-- left end -->
-    <!-- right -->
-    <div class='col-lg-12 col-md-9 col-sm-9 col-xs-12'>
-      <div class="order-tab">
-        <h5 class="page-title">
-					我的购物车
-				</h5>
-        <div id="myTabContent" class="tab-content">
-            <?php if(!empty($list)):?>
+<?$this->load->view('home/header-cart')?> 
+<div id="mycart">
+  <div class='container m-t-20' > 
+    <div class='row'>
+      <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+        <div class="order-tab">
+          <h5 class="page-title">
+  					<span>我的购物车</span>
+  				</h5>
+          <div id="myTabContent" class="tab-content">
             <table class="table table-striped table-advance table-hover">
-                <thead>
-                    <tr>
-                      	<th width="130">
-                            <input value="option1" id="inlineCheckbox1" type="checkbox" title='全选'>
-                        </th>
-                        <th>商品</th>
-                        <th width="150">价格</th>
-                        <th width="120">优惠</th>
-                        <th width="150">数量</th>
-                        <th width="150">操作</th>  
-                    </tr>
+              <thead>
+                <tr>
+                	<th width="90"> <label class="left"><input type="checkbox"> 全选</label> </th>
+                  <th width="460">商品</th>
+                  <th width="160">价格</th>
+                  <th width="120">优惠</th>
+                  <th width="150">数量</th>
+                  <th width="140">操作</th>  
+                </tr>
                 </thead>
                 <tbody> 
-                    <?php foreach ($list as $key => $value) :?>
-                    <tr id='cart_row_<?php echo $value['product_id']?>'>
-                        <td><span><input value="option1" id="inlineCheckbox1" type="checkbox"></span></td> 
-                        <td><?php echo $value['name']?></td> 
-                        <td><?php echo $value['price']?></td>
-                        <td><?php echo $value['best_price']?></td> 
-                        <td><?php echo $value['count']?></td> 
-                        <td>
-                            <button class='btn red btn-xs' onclick="confirm_dialog('<?php echo $this->lang->line('delete_confirm')?>', '<?php echo $this->lang->line('sure_to_delete')?>', delCart, '<?php echo $value['product_id']?>')">
-                                <i class='fa fa-trash-o'></i>
-                            </button>
-                        </td> 
-                    </tr>
-                    <?php endforeach;?>
+                 <tr>
+                  <td ><div class="left"><span class="select-item"><input value="option1" id="inlineCheckbox1" type="checkbox"> <img class='box' src="<?php echo base_url();?>assets/img/home/cart-item.jpg"></span></div></td> 
+                  <td><div class="left pro-title">EGStyle原创设计 连衣裙夏2014 欧美风 宽松中长袖</div>  <div class="left pro-title">个性休闲 裙子</div><div class="pro-mark">[赠品] 50元电子京券（订单完成后自动发放）</div></td>
+                  <td><span class="pro-price">&#165;998.00</span></td> <td><sapn class="pro-price2">&#165;998.00</sapn></td> 
+                  <td><div class="pinfo">
+                          <input type="text" name="cart_num" value="33" class=" form-control">
+                          <button class="btn btn-default plus" onclick="cart_count('plus',this)">+</button>
+                          <button class="btn btn-default minus" onclick="cart_count('minus',this)">-</button>
+                          <input type="hidden" id="min_num" name="min_num" value="33">
+                      </div>
+                  </td> 
+                  <td><a herf="">删除</a></td>
+                 </tr>
+                 <tr>
+                  <td ><div class="left"><span class="select_item"><input value="option1" id="inlineCheckbox1" type="checkbox"> <img class='box' src="<?php echo base_url();?>assets/img/home/cart-item.jpg"></span></div></td> 
+                  <td><div class="left pro-title">EGStyle原创设计 连衣裙夏2014 欧美风 宽松中长袖</div>  <div class="left pro-title">个性休闲 裙子</div><div class="pro-mark">[赠品] 50元电子京券（订单完成后自动发放）</div></td>
+                  <td><span class="pro-price">&#165;998.00</span></td> <td><sapn class="pro-price2">&#165;998.00</sapn></td> 
+                  <td><div class="pinfo">
+                          <input type="text" name="cart_num" value="33" class=" form-control">
+                          <button class="btn btn-default plus" onclick="cart_count('plus',this)">+</button>
+                          <button class="btn btn-default minus" onclick="cart_count('minus',this)">-</button>
+                          <input type="hidden" id="min_num" name="min_num" value="33">
+                      </div>
+                  </td> 
+                  <td><a herf="">删除</a></td>
+                 </tr>               
+                 <tr>
+                  <td ><div class="left"><label class="left"><input type="checkbox"> 全选</label> </div></td> 
+                  <td><div class="left"><i class="fa fa-trash-o"></i> 清空</div></td>
+                  <td></td> 
+                  <td>
+                  </td> 
+                  <td><div class="total m-b-5"><span class="num">2</span> 件商品 总计：</div><div class="total">优惠：</div></td>
+                  <td><div class="m-b-5">&#165;998.00</div><div class="">&#165;998.00</div></td>
+                 </tr>
                 </tbody>
-            </table> 
-            <?php else:?>
-                <p style="text-align:center;padding:50px 0;">购物车为空，继续购买商品 <a href="<?php echo base_url();?>">商城首页</a></p>
-            <?php endif;?>
-           
-        
-          <div class="tab-pane fade" id="profile"></div>
-          <div class="tab-pane fade" id="dropdown1"></div>
-          
+              </table>
+              <div class="toorder">总计( 不含运费 ) :<span class="order_price">&#165;998.00</span><button type="button" class="btn btn-danger">去结算</button></div>
+            <div class="tab-pane fade" id="profile"></div>
+            <div class="tab-pane fade" id="dropdown1"></div>          
+            <p style="text-align:center">购物车为空，继续购买商品 <a href="<?php echo base_url();?>">商城首页</a></p>     
+          </div>
         </div>
-      </div>
-
+      </div>  
     </div>
-    <!-- right end --> </div>
-</div>
+  </div>
+  <div class="container">  
+     <div id="recommend">
+      <div class="title">购买了同样商品的顾客还购买了</div>
+          <div class="img">
+            
+              <div class="text-center m-b-20">
+                  <div class="pinfo-s m-r-5 ">
+                      <img src="<?php echo base_url();?>assets/img/home/recommend1.jpg" class="item-thumb img-responsive">
+                      <div class="recommend-title">test</div>
+                      <div class="recommend-price">&#165;998.00</div>
+                  </div>
+                  <div class="pinfo-s m-r-5">
+                      <img src="<?php echo base_url();?>assets/img/home/recommend1.jpg" class=" img-responsive">
+                      <div class="recommend-title">test</div>
+                      <div class="recommend-price">&#165;998.00</div>
+                  </div>
+                  <div class="pinfo-s m-r-5">
+                      <img src="<?php echo base_url();?>assets/img/home/recommend1.jpg" class="item-thumb img-responsive">
+                      <div class="recommend-title">test</div>
+                      <div class="recommend-price">&#165;998.00</div>
+                  </div>
+                  <div class="pinfo-s m-r-5">
+                      <img src="<?php echo base_url();?>assets/img/home/recommend1.jpg" class="item-thumb img-responsive">
+                      <div class="recommend-title">test</div>
+                      <div class="recommend-price">&#165;998.00</div>
+                  </div>
+                  <div class="pinfo-s">
+                      <img src="<?php echo base_url();?>assets/img/home/recommend1.jpg" class="item-thumb img-responsive">
+                      <div class="recommend-title">test</div>
+                      <div class="recommend-price">&#165;998.00</div>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div>
+  </div>
+
 
 <div class='container m-b-20' id='ad-footer'>
   <div class=''>
---
+++ app/views/home/header-cart.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+    <meta charset="utf-8" />
+    <title>One </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <meta name="MobileOptimized" content="320">
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->          
+    <link href="<?php echo base_url();?>assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url();?>assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN THEME STYLES --> 
+    <link href="<?php echo base_url();?>assets/css/style-metronic.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url();?>assets/css/home.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url();?>assets/css/custom.css" rel="stylesheet" type="text/css"/>
+    <!-- END THEME STYLES -->
+    <link rel="shortcut icon" href="favicon.ico" />
+    <script src="<?php echo base_url();?>assets/plugins/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script>
+        var msg = {
+            'base_url':"<?php echo base_url();?>"
+        };
+    </script>
+    <script src="<?php echo base_url();?>assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+    <!-- BEGIN CORE PLUGINS -->   
+    <!--[if lt IE 9]>
+    <script src="<?php echo base_url();?>assets/plugins/respond.min.js"></script>
+    <script src="<?php echo base_url();?>assets/plugins/excanvas.min.js"></script> 
+    <![endif]-->
+    <script src="<?php echo base_url();?>assets/scripts/common.js" type="text/javascript"></script>
+    <!-- END JAVASCRIPTS -->
+</head>
+<!-- END HEAD -->
+<!-- BEGIN BODY -->
+<body class="clearfix">
+    <!-- begin top -->
+    <div id='top'>
+    <div  class='container'> 
+        <div class='row'>
+        <div class="col-md-4 m-t-3">收藏壹心易购</div>
+        <div class="text-right col-md-8 m-t-8">           
+            <?php if($this->auth->user_id()):?>
+            <span>欢迎您 <a href="<?php echo base_url().'users/index/'.$this->auth->user_id();?>"><?php echo $this->auth->username();?></a></span>
+            <span><a href="<?php echo base_url().'orders'?>">我的订单</a></span>
+            <?endif;?>
+            <span><a href="<?php echo base_url()?>/login">登录</a> | <a href="<?php echo base_url()?>register">注册</a></span>
+            </div>
+        </div>
+   </div>
+</div>
+<!-- end top -->
+<!-- header -->
+<div id='' class='container m-t-20'>
+    <div class='row'>
+        <div class="col-md-4 m-t-10 m-b-10">
+            <a href="#">
+              <img src="<?php echo base_url()?>assets/img/logo.png">
+            </a>
+        </div>
+        <div class="col-md-8 col-xs-12">
+            jlkjlj
+        </div>
+    </div>
+</div> 
+
+<div class="clearfix"></div>
+    <!-- header end -->
