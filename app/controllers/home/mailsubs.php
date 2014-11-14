df974a46345ced312ffa88f1be34f93cf1fce1a9
alex.liang <jiahuai.liang@iaspec.net>
2014-11-14 15:46:42 +0800

    前台我的邮件订阅修改

--- app/controllers/home/mailsubs.php
+++ app/controllers/home/mailsubs.php
@@ -6,11 +6,28 @@ class Mailsubs extends CI_Controller {
 	 * Index Page for this controller.
 	 * 
 	 */
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('user');
+    }
+
+	/**
+	 * Index Page for this controller.
+	 * 
+	 */
 
 	public function index()
 	{
-	
-		$this->load->view('home/mailsubs');
+		$this->auth->check_login();
+        $user_id = $this->auth->user_id();
+		if(!$user_id)
+		{
+			show_404('',false);
+		}
+		$user = $this->user->get($user_id);
+		$data['user'] = $user;
+		$this->load->view('home/mailsubs',$data);
 	}
 
 }
